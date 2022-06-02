8a453c507f303338c4e3d0622cffd14519cbcdc0
AndสrᎥlh๏ ⁧ <50175239+and4r1lh0@users.noreply.github.com>
2022-06-02 23:07:19 +0400

    Update navlock.py

--- navlock.py
+++ navlock.py
@@ -1,17 +1,22 @@
+#def ipInfo(addr=''):
+#    from urllib.request import urlopen
+#    from json import load
+#    if addr == '':
+#        url = 'https://ipinfo.io/json'
+#    else:
+#        url = 'https://ipinfo.io/' + addr + '/json'
+#    res = urlopen(url)
+#    #response from url(if res==None then check connection)
+#    data = load(res)
+#    #will load the json response into data
+#    mas=[]
+#    for attr in data.keys():
+#        #will print the data line by line
+#        mas +=(attr,data[attr])
+#    #print(mas)
+#    return mas
+
 def ipInfo(addr=''):
-    from urllib.request import urlopen
-    from json import load
-    if addr == '':
-        url = 'https://ipinfo.io/json'
-    else:
-        url = 'https://ipinfo.io/' + addr + '/json'
-    res = urlopen(url)
-    #response from url(if res==None then check connection)
-    data = load(res)
-    #will load the json response into data
-    mas=[]
-    for attr in data.keys():
-        #will print the data line by line
-        mas +=(attr,data[attr])
-    #print(mas)
-    return mas
+    from ip2geotools.databases.noncommercial import DbIpCity
+    response = DbIpCity.get(addr, api_key='free')
+    return response.country
