850c02babe4ca933b40396093ac5812ff9df5268
AndสrᎥlh๏ ⁧ <50175239+and4r1lh0@users.noreply.github.com>
2022-06-02 23:08:19 +0400

    Update portscan.py

--- portscan.py
+++ portscan.py
@@ -2,7 +2,7 @@ import socket
 
 def scan_port(ip,port):
   sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-  sock.settimeout(0.5)
+  sock.settimeout(0.1) #set timeout in sec 
   try:
      connect = sock.connect((ip,port))
      #print('Port :',port,' its open.')
