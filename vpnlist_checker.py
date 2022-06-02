5f532a93cedae26e8dc596249bf22cf96a8422c3
AndสrᎥlh๏ ⁧ <50175239+and4r1lh0@users.noreply.github.com>
2022-06-02 22:39:49 +0400

    Add files via upload

---
+++ vpnlist_checker.py
@@ -0,0 +1,56 @@
+import base64
+import re
+import ping
+#import simple_ping as s
+import navlock
+from portscan import scan_port
+
+def proc(str):
+    sharp_replace = str.partition('#')[0]
+    result = sharp_replace.replace('ss://', '', 1)
+    return result
+
+def parse(encstr):
+    decoded_uri = base64.b64decode(encstr.encode('utf-8'))
+    return('%s' % decoded_uri.decode('utf-8'))
+
+i=1
+file = open("shadowsocks_full.txt", "r")
+file2 = open("out.txt", "w")
+
+while True:
+    line = file.readline()
+    if not line:
+        break
+    cur_line = (line.strip())
+    proc_line = proc(cur_line)
+    parsed_line = parse(proc_line)
+    double_parse = parsed_line.partition(':')[-1]
+    port = double_parse.partition(':')[-1]
+    #print(parsed_line)
+    ip_add = (re.search(r'(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)',parsed_line))
+    ip_address = ip_add[0]
+    ping_attempt = scan_port(ip_address,int(port))
+
+    #ping_attempt = s.myping(ip_add[0])
+
+    #if ping_attempt==None:
+    #    pass
+    #else: 
+
+    if ping_attempt==None:
+        pass
+    else:
+        pinga = ping.main(ip_address)
+        #country = navlock.ipInfo(ip_add[0])
+        #country = country[9]
+        country = 'Null'
+        #pinga = None
+        if pinga == None:
+            pinga = [0,'Err']
+        print('№ ',i,'\tIP: ',ip_add[0],'\t| Пинг: ',pinga[1],'\t| Страна: ',country)
+        i+=1
+        #file2.write(line + '\n')
+        file2.writelines(line)
+file2.close()
+file.close()
